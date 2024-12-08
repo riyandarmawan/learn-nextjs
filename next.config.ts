08f292be75c1633ba0b2ac9b250eb3383539d3dc
riyandarmawan <riyandaramwan042@gmail.com>
2024-12-08 07:53:51 +0700

    unoptimized image true

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
